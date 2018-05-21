1bb767d51fce42190492a5b1edb70cbb0f8716b6
Keren Nipomnyashih <keren@sears.co.il>
2018-05-21 20:27:06 +0300

    Adding google-services.json
    Adding first login screen
    Addings images

--- app/src/main/java/com/drive2study/LoginScreen.java
+++ app/src/main/java/com/drive2study/LoginScreen.java
@@ -3,13 +3,12 @@ package com.drive2study;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.drive2study.Model.Model;
 
-public class MainActivity extends Activity {
+public class LoginScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_screen);
     }
 }
